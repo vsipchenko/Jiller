97fe70a68cdeabb4bf3861e3c6f5314f2f8ef229
Norfolkik <norfolknorfolks@gmail.com>
2017-01-21 15:15:39 +0200

    Added atribute AUTH_USER_MODEL to settings.py; added *.sqlite3 to gitignore; added class Employee and import of AbstractUser to workflow models.py

--- workflow/models.py
+++ workflow/models.py
@@ -1,23 +1,29 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 class Project(models.Model):
-	pass
+    pass
 
 
-class Sprint(modles.Model):
-	pass
+class Sprint(models.Model):
+    pass
+
 
 class Issue(models.Model):
-	pass
+    pass
 
 
 class ProjectTeam(models.Model):
-	pass
+    pass
 
-class User(models.Model):
-	pass
+
+class Employee(AbstractUser):
+    role = models.CharField(max_length=255)
+
 
 class IssueLog(models.Model):
-	pass
+    pass
+
