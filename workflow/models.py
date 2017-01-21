5f66c72b413d319089ef3f059bd54de1a0fa94f1
Norfolkik <norfolknorfolks@gmail.com>
2017-01-21 16:24:28 +0200

    Finished models: Employee, Team, IssueLog; created first migration;

--- workflow/models.py
+++ workflow/models.py
@@ -1,12 +1,17 @@
 from __future__ import unicode_literals
 
-from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 from django.core.validators import MaxValueValidator
 
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
+class Employee(AbstractUser):
+    role = models.CharField(max_length=255, verbose_name=_('Role'))
+
+    def __str__(self):
+        return self.username
 
 
 class Project(models.Model):
@@ -30,15 +35,15 @@ class Sprint(models.Model):
     )
     title = models.CharField(verbose_name=_('Title'), max_length=255)
     project = models.ForeignKey(Project)
-    team = models.ForeignKey(ProjectTeam)
+    team = models.ForeignKey('ProjectTeam')
     start_date = models.DateField(verbose_name=_('Start date'), null=True, blank=True)
     end_date = models.DateField(verbose_name=_('End date'), null=True, blank=True)
     order = models.PositiveIntegerField(verbose_name=_('Order'), null=True, blank=True)
-    status = models.CharField(verbose_name=_('Status'), choices=SPRINT_STATUS_CHOICES, null=True, blank=True)
+    status = models.CharField(verbose_name=_('Status'), choices=SPRINT_STATUS_CHOICES, null=True, blank=True,
+                              max_length=255)
 
     def __str__(self):
         return self.title
-
 
 
 class Issue(models.Model):
@@ -53,12 +58,12 @@ class Issue(models.Model):
     root = models.ForeignKey('self', null=True, blank=True)
     project = models.ForeignKey(Project, verbose_name=_('Project'))
     sprint = models.ForeignKey(Sprint, verbose_name=_('Sprint'))
-    author = models.ForeignKey(User, verbose_name=_('Author'))
-    employee = models.ForeignKey(User, verbose_name=_('Employee'))
+    author = models.ForeignKey(Employee, verbose_name=_('Author'), related_name='author_user')
+    employee = models.ForeignKey(Employee, verbose_name=_('Employee'), related_name='worker_user')
     title = models.CharField(verbose_name=_('Title'), max_length=255)
     description = models.TextField(verbose_name=_('Description'), null=True, blank=True)
     status = models.CharField(verbose_name=_('Status'), choices=ISSUE_STATUS_CHOICES, default=NEW, null=True,
-                              blank=True)
+                              blank=True, max_length=255)
     estimation = models.PositiveIntegerField(verbose_name=_('Estimation'), validators=[MaxValueValidator(240)],
                                              null=True, blank=True)
 
@@ -67,12 +72,29 @@ class Issue(models.Model):
 
 
 class ProjectTeam(models.Model):
-    pass
+    project = models.ForeignKey(Project, verbose_name=_('Project'))
+    title = models.CharField(max_length=255, verbose_name=_('Title'))
+    employees = models.ManyToManyField(Employee, verbose_name=_('Employees'))
 
-
-class Employee(AbstractUser):
-    role = models.CharField(max_length=255)
+    def __str__(self):
+        return self.title
 
 
 class IssueLog(models.Model):
-	pass
+    CREATED = 'created'
+    CHANGED = 'changed'
+    RESOLVED = 'resolved'
+    ACTION_TYPES_CHOICES = (
+        (CREATED, _('Created')),
+        (CHANGED, _('Changed')),
+        (RESOLVED, _('Resolved'))
+    )
+
+    issue = models.ForeignKey(Issue, verbose_name=_('Issue'))
+    action = models.CharField(verbose_name=_('Action'), choices=ACTION_TYPES_CHOICES, default=CREATED, max_length=255)
+    user = models.ForeignKey(Employee, verbose_name=_('Employee'))
+    time = models.DateTimeField(verbose_name=_('Time'), auto_now_add=True)
+    data = models.CharField(verbose_name=_('Action data'), max_length=255)
+
+    def __str__(self):
+        return "{}:{}".format(self.issue, self.action, self.user)
