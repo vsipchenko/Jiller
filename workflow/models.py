aae0320e92636b34006f0c6f06c216491b267328
Norfolkik <norfolknorfolks@gmail.com>
2017-01-21 13:45:56 +0200

    Created base class names.

--- workflow/models.py
+++ workflow/models.py
@@ -2,4 +2,22 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+	pass
+
+
+class Sprint(modles.Model):
+	pass
+
+class Issue(models.Model):
+	pass
+
+
+class ProjectTeam(models.Model):
+	pass
+
+class User(models.Model):
+	pass
+
+class IssueLog(models.Model):
+	pass
