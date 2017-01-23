35194172a2e13e041c808f2d33294736d8c7c794
italamanova <iryna.talamanova@gmail.com>
2017-01-23 20:00:14 +0200

    Initial commit

--- workflow/urls.py
+++ workflow/urls.py
@@ -1,9 +1,19 @@
 from django.conf.urls import url
 
 from . import views
+from workflow.views import ProjectDetail, ProjectCreate
 
 app_name = 'workflow'
 urlpatterns = [
     url(r'^', views.index, name='index'),
+
+    #/project/[pr_id]/
+    url(r'^/project/(?P<project_id>[0-9]+)/$', ProjectDetail.as_view(), name='project_detail'),
+
+    # /project/create/
+    url(r'^/project/create/$', ProjectCreate.as_view(), name='project_create'),
+
+    # /project/update/[pr_id]/
+    #url(r'^/project/update/(?P<project_id>[0-9]+)/$', views.project_update, name='project_update'),
 
 ]
--- workflow/views.py
+++ workflow/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views import generic
+from django.utils import timezone
+from workflow.models import Project
 
 
 def index(request):
     return render(request, 'workflow/index.html')
+
+
+# def project(request, project_id):
+#     return render(request, 'workflow/project_detail.html')
+
+class ProjectDetail(generic.DetailView):
+    model = Project
+    template_name = 'workflow/project_detail.html'
+
+
+class ProjectCreate(CreateView):
+    model = Project
+    fields = ['title', 'start_date', 'end_date']
