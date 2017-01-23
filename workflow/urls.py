9d95e8f598de19e176c41831716bc18fdfcc9f6a
Phobos <phobosprogrammer@gmail.com>
2017-01-24 01:36:17 +0200

    Add sprint page information

--- workflow/urls.py
+++ workflow/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 app_name = 'workflow'
 urlpatterns = [
-    url(r'^', views.index, name='index'),
-
+    url(r'^$', views.index, name='index'),
+    url(r'^project/(?P<project_id>[0-9]+)/issue/(?P<issue_id>[0-9]+)/$',
+        views.issue, name='issue'),
+    url(r'^project/(?P<project_id>[0-9]+)/sprint/(?P<sprint_id>[0-9]+)/$',
+        views.SprintView.as_view(), name='sprint'),
 ]
--- workflow/views.py
+++ workflow/views.py
@@ -1,5 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.views import generic
+
+from .models import Issue, Sprint
+
+# Create your views here.
 
 
 def index(request):
     return render(request, 'workflow/index.html')
+
+
+def issue(request, project_id, issue_id):
+    current_issue = get_object_or_404(Issue, pk=issue_id, project=project_id)
+    return render(request, 'workflow/issue.html', {
+        'issue': current_issue, 'project_id': project_id, 'issue_id': issue_id
+    })
+
+
+class SprintView(generic.DetailView):
+    model = Sprint
+    template_name = 'workflow/sprint.html'
+    query_pk_and_slug = True
+    pk_url_kwarg = 'sprint_id'
+    slug_field = 'project'
+    slug_url_kwarg = 'project_id'
+
+    def get_context_data(self, **kwargs):
+        context = super(SprintView, self).get_context_data(**kwargs)
+        cur_proj = self.kwargs['project_id']
+        cur_spr = self.kwargs['sprint_id']
+        issues_from_this_sprint = Issue.objects.filter(project_id=cur_proj,
+                                                       sprint_id=cur_spr)
+        context['new_issues'] = issues_from_this_sprint.filter(status="new")
+        context['in_progress_issues'] = \
+            issues_from_this_sprint.filter(status="in progress")
+        context['resolved_issues'] = \
+            issues_from_this_sprint.filter(status="resolved")
+        return context
