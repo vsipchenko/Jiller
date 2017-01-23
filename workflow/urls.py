12d6dd1dff864040bf9ea73f4600f5560b4c88bb
Norfolkik <norfolknorfolks@gmail.com>
2017-01-23 11:13:31 +0200

    Changed urls to accept llogin and registration templates; added base views to handle them.

--- workflow/urls.py
+++ workflow/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'workflow'
 urlpatterns = [
 
-    url(r'^', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^registration/$',views.registration, name='registration')
 ]
--- workflow/views.py
+++ workflow/views.py
@@ -6,3 +6,10 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'workflow/index.html')
 
+
+def login(request):
+    return render(request, 'workflow/login.html')
+
+def registration(request):
+    return render(request, 'workflow/registration.html')
+
