43e971f42f3c589989538907570fa16534cdad22
Norfolkik <norfolknorfolks@gmail.com>
2017-01-23 21:00:24 +0200

    Created login and registration html, made simple authentification; cofigured ursl for the templates and views.

--- workflow/urls.py
+++ workflow/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 app_name = 'workflow'
 urlpatterns = [
-    url(r'^', views.index, name='index'),
-
+    url(r'^$', views.index, name='index'),
+    url(r'^login/$', views.login_form, name='login'),
+    url(r'^login_proceed/$', views.login_proceed, name='login_proceed'),
+    url(r'^registration/$', views.registration_form, name='registration'),
+    url(r'^registration_proceed/$', views.registration_proceed, name='registration_proceed'),
 ]
--- workflow/views.py
+++ workflow/views.py
@@ -1,5 +1,40 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.utils.translation import ugettext_lazy as _
+from django.shortcuts import render, redirect
+
+from workflow.models import Employee
 
 
 def index(request):
     return render(request, 'workflow/index.html')
+
+
+def login_form(request):
+    return render(request, 'workflow/login.html')
+
+
+def login_proceed(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect('workflow:profile')
+    else:
+        messages.error(request, _("Wrong username or password"))
+        return redirect('workflow:login')
+
+
+def registration_form(request):
+    return render(request, 'workflow/registration.html')
+
+
+def registration_proceed(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    last_name = request.POST['last_name']
+    first_name = request.POST['first_name']
+    email = request.POST['email']
+    empl = Employee.objects.create_user(username, email, password, last_name=last_name, first_name=first_name)
+    return redirect('workflow:profile')
