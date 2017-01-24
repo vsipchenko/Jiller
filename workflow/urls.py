77df189b2b58b21660769c48a21abc554be6fa5b
Alex <oleksandrdubrovskyi@gmail.com>
2017-01-24 13:30:46 +0200

    add backlog list

--- workflow/urls.py
+++ workflow/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'workflow'
 urlpatterns = [
     url(r'^', views.index, name='index'),
+    url(r'^project/(?P<pr_id>[0-9]+)/backlog/$', views.backlog, name='backlog'),
 
 ]
--- workflow/views.py
+++ workflow/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import Project, Issue
 
 
 def index(request):
     return render(request, 'workflow/index.html')
+
+
+def backlog(request, pr_id):
+    project = Project.objects.filter(pk=pr_id)
+    issues = Issue.objects.filter(project=pr_id).filter(status__isnull=True)
+
+    return render(request, 'workflow/backlog.html', {'project': project,
+                                                     'issues': issues})
