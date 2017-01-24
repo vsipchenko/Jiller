2f9a5e12b43afdebaa23b9a38666313c1a479340
Alex <oleksandrdubrovskyi@gmail.com>
2017-01-24 13:19:33 +0200

    add sprints_list

--- workflow/urls.py
+++ workflow/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 app_name = 'workflow'
 urlpatterns = [
-    url(r'^', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
+    url(r'^project/(?P<pr_id>[0-9]+)/sprint/$', views.sprints_list, name='sprints_list'),
 
 ]
--- workflow/views.py
+++ workflow/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import Project, Sprint
 
 
 def index(request):
     return render(request, 'workflow/index.html')
+
+
+def sprints_list(request, pr_id):
+    project = Project.objects.filter(pk=pr_id)
+    sprints = Sprint.objects.filter(project=pr_id)
+
+    return render(request, 'workflow/sprints_list.html', {'project': project,
+                                                          'sprints': sprints})
