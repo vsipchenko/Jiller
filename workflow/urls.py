74c65908594575361d0c1e31a42655c79a268e5c
egepsihora <zeland18@ukr.net>
2017-01-24 11:59:45 +0200

    issue create and edit pages

--- workflow/urls.py
+++ workflow/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 app_name = 'workflow'
 urlpatterns = [
-    url(r'^', views.index, name='index'),
-
+    url(r'^$', views.index, name='index'),
+    url(r'^project/(?P<project_id>\w+)/issue/create/$', views.create_issue, name='create_issue'),
+    url(r'^project/(?P<project_id>\w+)/issue/(?P<issue_id>\w+)/edit/$', views.edit_issue, name='edit_issue'),
+    url(r'^project/(?P<project_id>\w+)/team/$', views.team, name='team'),
 ]
--- workflow/views.py
+++ workflow/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Project, ProjectTeam, Issue
 
 
 def index(request):
     return render(request, 'workflow/index.html')
+
+
+def create_issue(request, project_id):
+    return render(request, 'workflow/create_issue.html', {'project_id': project_id})
+
+
+def edit_issue(request, project_id, issue_id):
+    return render(request, 'workflow/edit_issue.html', {'project_id': project_id, 'issue_id': issue_id})
+
+
+def team(request, project_id):
+
+    return render(request, 'workflow/team.html', {'project_id': project_id})
+
+
+def not_found(request):
+    return render(request, 'workflow/not_found.html')
