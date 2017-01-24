50b8fdb175f7a2097e1f53c1f699e7e030de8826
Norfolks <norfolknorfolks@gmail.com>
2017-01-24 13:32:14 +0200

    Reg and log pages init (#9)
    * Created login and registration html, made simple authentification; cofigured ursl for the templates and views.
    
    * Rewrite Login and Registration Pages using Django Forms

--- workflow/urls.py
+++ workflow/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 app_name = 'workflow'
 urlpatterns = [
-    url(r'^', views.index, name='index'),
-
+    url(r'^$', views.index, name='index'),
+    url(r'^login/$', views.login_form, name='login'),
+    url(r'^registration/$', views.registration_form, name='registration'),
 ]
