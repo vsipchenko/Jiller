37d00844654adf85cbf61e33fef57834542a5695
egepsihora <zeland18@ukr.net>
2017-01-25 11:42:52 +0200

    team_list, creaye_issue, edit_issue

--- workflow/urls.py
+++ workflow/urls.py
@@ -1,36 +1,11 @@
 from django.conf.urls import url
-
 from . import views
-from workflow.views import ProjectUpdate, ProjectCreate, ProjectDelete, ProjectDetail
 
-app_name = 'workflow'
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
 
     url(r'^project/(?P<project_id>\w+)/issue/create/$', views.create_issue, name='create_issue'),
     url(r'^project/(?P<project_id>\w+)/issue/(?P<issue_id>\w+)/edit/$', views.edit_issue, name='edit_issue'),
     url(r'^project/(?P<project_id>\w+)/team/$', views.team, name='team'),
-  
-    url(r'^project/(?P<pr_id>[0-9]+)/backlog/$', views.backlog, name='backlog'),
-  
-    url(r'^project/(?P<project_id>[0-9]+)/issue/(?P<issue_id>[0-9]+)/$',
-        views.issue, name='issue'),
-  
-    url(r'^project/(?P<project_id>[0-9]+)/sprint/(?P<sprint_id>[0-9]+)/$',
-        views.SprintView.as_view(), name='sprint'),
-
-    url(r'^login/$', views.login_form, name='login'),
-    url(r'^registration/$', views.registration_form, name='registration'),
-
-    #/project/[pr_id]/
-    url(r'^project/(?P<pk>[0-9]+)/$', ProjectDetail.as_view(), name='project_detail'),
-
-    # /project/create/
-    url(r'^project/create/$', ProjectCreate.as_view(), name='project_create'),
-
-    # /project/update/[pr_id]/
-    url(r'^project/update/(?P<pk>\d+)/$', ProjectUpdate.as_view(), name='project_update'),
-
-    # /project/update/[pr_id]/
-    url(r'^project/delete/(?P<pk>\d+)/$', ProjectDelete.as_view(), name='project_delete'),
 ]
